4e107d6744a45f154c0417907dc382aa11a8a5f8
Bobby Brice <bobby.brice@gmail.com>
2020-04-13 20:14:13 -0400

    updated project to remove jQuery and fix bugs

--- scripts.js
+++ scripts.js
@@ -1,88 +1,64 @@
-$(function() { //document ready begin
-
-window.onload = function() {
-	  let open = document.getElementById('open');
-	  open.style.marginLeft = "25px"; //opening header slide in
-	};
-
-	$("h1, h3").fadeOut(2000);
-	$("h1, h3").fadeIn(2000); // fade in and out header
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 
-	const $counter = $('span.counter')
-	let currentInterval;
+function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
 
-	function startTimer(duration, display) {
-	    let timer = duration, minutes, seconds;
-	    currentInterval = setInterval(function () {
-	        minutes = parseInt(timer / 60, 10)
-	        seconds = parseInt(timer % 60, 10);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
 
-	        minutes = minutes < 10 ? "0" + minutes : minutes;
-	        seconds = seconds < 10 ? "0" + seconds : seconds;
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it!
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
 
-	        display.text(minutes + ":" + seconds);
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
 
-	        if (--timer < 0) {
-	            timer = duration;
-	        }
-	    }, 1000);
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+	const switchImg = document.querySelector('#switchImg');
+	if (seconds === 3600) {
+		switchImg.setAttribute('src', 'assets/image-4.jpg');
+	} else if (seconds === 1200) {
+			switchImg.setAttribute('src', 'assets/image-5.jpg');
+	} else if (seconds === 900) {
+		switchImg.setAttribute('src', 'assets/image-2.jpg');
+  } else if (seconds === 300) {
+  	switchImg.setAttribute('src', 'assets/image-4.jpg')
 	}
+}
+buttons.forEach(button => button.addEventListener('click', startTimer));
 
-	$(function() { // click functions to add/sub/start/stop and swap out images for work/lunch/break and party
-
-		$('.countdown').on('click', function() {
-			clearInterval(currentInterval);
-			 const time = $(this).data('time');
-
-			 if (time === 15) {
-			 	$('#switchImg').attr('src','assets/image-3.jpg');
-			 } else if (time === 30) {
-			 	$('#switchImg').attr('src','assets/image-5.jpg');
-			 } else if (time === 45) {
-			 	$('#switchImg').attr('src','assets/image-2.jpg');
-			 } else {
-			 	$('#switchImg').attr('src','assets/image-4.jpg');
-			 }
-
-			 const countdownNumber = 60 * time;
-			 display = $('#returnTime');
-			 startTimer(countdownNumber, display);
+// buttons.forEach(button => button.addEventListener('click', function (e) {
+// 	const time = this.data-time;
 
 
-		// $('#add').on('click', function() {
-		// 	    	let add = 60 * 1;
-		// 	        display = $('#returnTime');
-		// 	    	startTimer(add, display);
-
-		// }); // doesn't work with setInterval function :(
-
-		// $('#subtract').on('click', function() {
-		// 	    	let subtract = 60 * 1,
-		// 	        display = $('#returnTime');
-		// 	    	startTimer(subtract, display);
-		
-		// }); // doesn't work with the setInterval function
-		 
-		$('.party').on({
-		     'click': function(){
-		        $('#switchImg').attr('src','assets/image-4.jpg');
-		        clearInterval(currentInterval);
-		        $counter.text("Party all the time!");
-		     }
-		 });
-		 
-
-		$('#stop').on('click', function() {
-			clearInterval(currentInterval);
-		});	
-
-		$('#start').on('click', function() {	
-		 startTimer(countdownNumber, display);
-			
-		});	
-	});	
-});	
-
-
-});	// document ready end
+// }));
