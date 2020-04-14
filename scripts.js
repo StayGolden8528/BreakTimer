aceed241d45f284b61ab897487f67232d5159913
Bobby Brice <bobby.brice@gmail.com>
2020-04-13 20:47:24 -0400

    added comments and cleaned up some spacing

--- scripts.js
+++ scripts.js
@@ -7,15 +7,15 @@ const buttons = document.querySelectorAll('[data-time]');
 function timer(seconds) {
   // clear any existing timers
   clearInterval(countdown);
-
+	// Gets the current date/time and the data-time in seconds is passed to our display and end time Fn's
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
   displayEndTime(then);
-
+	// setInterval Fn saved to a var so we can clear it at 0
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
+    // check if we should stop the timer
     if (secondsLeft < 0) {
       clearInterval(countdown);
       return;
@@ -25,6 +25,7 @@ function timer(seconds) {
   }, 1000);
 }
 
+// Determines the time remaining and sets the textContent to display that time - passed to the timer Fn
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
@@ -33,6 +34,7 @@ function displayTimeLeft(seconds) {
   timerDisplay.textContent = display;
 }
 
+// Determines the end time and sets the textContent to display that time - passed to the timer Fn
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
@@ -42,19 +44,25 @@ function displayEndTime(timestamp) {
 }
 
 function startTimer() {
-  const seconds = parseInt(this.dataset.time);
-  timer(seconds);
+	//takes the time in seconds from the dataset clicked
+	const seconds = parseInt(this.dataset.time);
+	//initializes the timer Fn
+	timer(seconds);
+	
+	//conditional statement to swap images given the particular time selected
 	const switchImg = document.querySelector('#switchImg');
 	if (seconds === 3600) {
 		switchImg.setAttribute('src', 'assets/image-4.jpg');
 	} else if (seconds === 1200) {
 			switchImg.setAttribute('src', 'assets/image-5.jpg');
-	} else if (seconds === 900) {
+	} else if (seconds === 2700) {
 		switchImg.setAttribute('src', 'assets/image-2.jpg');
   } else if (seconds === 300) {
   	switchImg.setAttribute('src', 'assets/image-3.jpeg')
 	}
 }
+
+//event listener on each button to trigger the startTimer Fn
 buttons.forEach(button => button.addEventListener('click', startTimer));
 
 
